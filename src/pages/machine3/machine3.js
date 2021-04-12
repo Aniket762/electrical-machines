3c7f2c2aa513a57e8390a0f3eb9a760ce2be9b17
SayantanMaiti <sayantanmaiti17@gmail.com>
2021-04-12 14:38:23 +0530

    other forms changed

--- src/pages/machine3/machine3.js
+++ src/pages/machine3/machine3.js
@@ -24,6 +24,16 @@ function machine() {
               <TextWrapper>
                 <Heading lightText={false}>Case C</Heading>
                 <Subtitle lightTextDesc={false}>Both machines are placed in opposite phase flux and there is air gap flux.</Subtitle>
+                <Subtitle>
+                <ul>
+                <li>f1 = Frequency of supply given to machine 1</li>
+                <li>f2 = Frequency of supply given to machine 2</li>
+                <li>s1 = Slip of operation of machine 1</li>
+                <li>s2 = Slip of operation of machine 2</li>
+                <li>p1 = Number of poles in machine 1</li>
+                <li>p2 = Number of poles in machine 2</li>
+                </ul>
+                </Subtitle>
               </TextWrapper>
             </InfoColumn>
             <InfoColumn>
