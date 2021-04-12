4d82e8c02936cf90f0dc64ce9e474c2643feb240
SayantanMaiti <sayantanmaiti17@gmail.com>
2021-04-12 20:53:44 +0530

    form changes

--- src/pages/machine1/machine1Form.js
+++ src/pages/machine1/machine1Form.js
@@ -41,23 +41,6 @@ function App() {
           placeholder="p1"
           className='boxes'
         />
-      </div>
-
-      <div className="number-inputs">
-        <input
-          type="number"
-          value={f2}
-          onChange={(e) => setf2(+e.target.value)}
-          placeholder="f2"
-          className='boxes'
-        />
-        <input
-          type="number"
-          value={s2}
-          onChange={(e) => sets2(+e.target.value)}
-          placeholder="s2"
-          className='boxes'
-        />
         <input
           type="number"
           value={p2}
@@ -66,6 +49,7 @@ function App() {
           className='boxes'
         />
       </div>
+
       <button onClick={formulate} className='button'>Find Speed</button>
 
       <h2>{speed}</h2>
--- src/pages/machine2/machine2.js
+++ src/pages/machine2/machine2.js
@@ -26,9 +26,7 @@ function machine() {
                 <Subtitle>
                 <ul>
                 <li>f1 = Frequency of supply given to machine 1</li>
-                <li>f2 = Frequency of supply given to machine 2</li>
                 <li>s1 = Slip of operation of machine 1</li>
-                <li>s2 = Slip of operation of machine 2</li>
                 <li>p1 = Number of poles in machine 1</li>
                 <li>p2 = Number of poles in machine 2</li>
                 </ul>
