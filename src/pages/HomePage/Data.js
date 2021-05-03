d1548243b17df533f2503da89ad271e03648d619
SayantanMaiti <sayantanmaiti17@gmail.com>
2021-05-03 13:01:00 +0530

    Update Data.js

--- src/pages/HomePage/Data.js
+++ src/pages/HomePage/Data.js
@@ -6,11 +6,12 @@ export const homeObjOne = {
   lightTextDesc: true,
   headline: 'Analyze the speed of your induction motor',
   description:
-    'Get speed of two slip ring induction motors for different types of cascading operation.',
+    'Get speed of two slip ring induction motors for different types of cascading operation. f1 = input frequency of voltage to MIM (Main Induction Motor) p1 = number of poles in MIM p2 = number of poles in AIM (Auxiliary Induction Motor) ',
   buttonLabel: 'Get Started',
   imgStart: '',
   img: require('../../images/home-main.svg'),
   alt: 'Credit Card',
+  linkTo:'/machine-2',
   start: ''
 };
 
@@ -20,9 +21,9 @@ export const homeObjTwo = {
   lightTopLine: false,
   lightText: false,
   lightTextDesc: false,
-  headline: 'Case A',
+  headline: 'Torque of both machines in same direction',
   description:
-    'Torque of both machines are in same direction.',
+    'The shaft of the motors are coupled with each other.',
   buttonLabel: 'Find Speed',
   imgStart: 'start',
   img: require('../../images/machine-2.svg'),
@@ -37,7 +38,7 @@ export const homeObjThree = {
   lightTopLine: true,
   lightText: true,
   lightTextDesc: true,
-  headline: 'Case B',
+  headline: 'Torque of both machines in different direction',
   description:
     "Torque of both machines are in different direction.",
   buttonLabel: 'Find Speed',
